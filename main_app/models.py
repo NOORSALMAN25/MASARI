03226614089dfb35754c29f188089214ce08bb98
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-26 13:00:58 +0300

    did the models for the universities

--- main_app/models.py
+++ main_app/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 class University(models.Model):
-    
-    
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    website_link = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
+    email = models.TextField(max_length=50)
+    phone = models.IntegerField(max_length=15)
+    image = models.ImageField(upload_to='main_app/static/uploads/', default='')
