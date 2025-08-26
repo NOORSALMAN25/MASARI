3adc2acbb804b6660e48821a2abc3ae750598569
Yara-Waleed <yarawaleed292@gmail.com>
2025-08-26 11:04:48 +0300

    done with the login and logout

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def home(request):
@@ -7,3 +9,18 @@ def home(request):
 
 def about(request):
     return render(request , 'about.html')
+
+def signup(request):
+    error_message = ""
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid Signup - Try Again...'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
