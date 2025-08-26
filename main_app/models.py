b36f0adc36202ee172a6b3e3fb4f6fcb8c21c3d9
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-26 13:11:37 +0300

    setup the answer model

--- main_app/models.py
+++ main_app/models.py
@@ -8,5 +8,13 @@ class Question(models.Model):
     question_txt = models.CharField(max_length=200)
     date_posted = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    answer_txt = models.CharField(max_length=200)    
+    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    question = models.ForeignKey(Question , on_delete=models.CASCADE , related_name='answers')
     
+
+
 
