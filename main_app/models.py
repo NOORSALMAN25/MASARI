675ca558b5af41bdbc94119f19e0a3947c6adca4
NOORSALMAN25 <041noorsalman@gmail.com>
2025-08-28 10:37:09 +0300

    done with map

--- main_app/models.py
+++ main_app/models.py
@@ -10,12 +10,15 @@ from django.contrib.auth.models import User
 
 class University(models.Model):
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    website_link = models.CharField(max_length=300)
-    description = models.TextField(max_length=300)
-    email = models.CharField(max_length=50)
-    phone = models.CharField()
-    image = models.ImageField(upload_to='main_app/static/uploads/', default='')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    website_link = models.URLField(max_length=300, blank=True)
+    description = models.TextField(max_length=300, blank=True)
+    email = models.EmailField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    image = models.ImageField(upload_to='universities/', blank=True, null=True)
+
 
     def __str__(self):
         return self.name
